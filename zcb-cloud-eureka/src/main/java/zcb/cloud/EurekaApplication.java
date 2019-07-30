0e83cae7939b797a24dc647291ec34b9c461497a
zhengchaobin <zhengchaobin@yunnex.com>
2019-07-30 15:38:37 +0800

    新增eureka服务注册中心

---
+++ zcb-cloud-eureka/src/main/java/zcb/cloud/EurekaApplication.java
@@ -0,0 +1,18 @@
+package zcb.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * created by @author zhengchaobin at 20190729
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
