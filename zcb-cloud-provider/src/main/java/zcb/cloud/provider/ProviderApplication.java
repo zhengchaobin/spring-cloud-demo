1a611c8d9004c79b0288db012ea91a281e81ebf2
zhengchaobin <zhengchaobin@yunnex.com>
2019-11-15 18:17:02 +0800

    增加websocket

--- zcb-cloud-provider/src/main/java/zcb/cloud/provider/ProviderApplication.java
+++ zcb-cloud-provider/src/main/java/zcb/cloud/provider/ProviderApplication.java
@@ -4,15 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @ComponentScan
 @EnableEurekaClient
+@Configuration
 @SpringBootApplication
 public class ProviderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
 
 }
