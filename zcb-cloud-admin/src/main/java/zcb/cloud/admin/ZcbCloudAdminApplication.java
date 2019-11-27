95a6f5b3cbbc26af14590772522aef4be9f163e7
zhengchaobin <zhengchaobin@yunnex.com>
2019-11-27 18:31:07 +0800

    集成spring-cloud-admin

---
+++ zcb-cloud-admin/src/main/java/zcb/cloud/admin/ZcbCloudAdminApplication.java
@@ -0,0 +1,17 @@
+package zcb.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableAdminServer
+@EnableEurekaClient
+@SpringBootApplication
+public class ZcbCloudAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZcbCloudAdminApplication.class, args);
+    }
+
+}
